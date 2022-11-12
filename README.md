f0a427b83343b9e51dd8ed44362f67985c00a8aa
Tony Diethelm <tonydiethelm@gmail.com>
2022-11-11 21:44:34 -0800

    Adding more documentation in README, some small spelling fixes.

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# holler
+# Holler
+
+This is meant to replace console logs in express routes and middleware. 
+You can place this in your express routes, then comment them out when you don't need them any more. 
+This saves you the time and effort of taking your console logs out of your code when you're done. 
+
+Holler lists:
+- request.body
+- request.params
+- request.query
+- response.locals
+- response.body
+
+Holler does not change your data in any way. It should not slow down your Express app in any significant way. It's just console logs. 
+
+## An example
+![Holler example](./holler.png)
+
+Now you can trace request.body, etc, as it moves through all your middleware, without clogging up your modules with console logs. 
+
+Holler is written in Javascript and uses CommonJS import/export for easy use in Node.js. 
+
+## Written by Tony Diethelm
+[github](https://github.com/tonydiethelm)
+[linked](https://www.linkedin.com/in/tonydiethelm)
---
+++ holler.js
@@ -0,0 +1,51 @@
+/*
+This is meant to replace console logs in express routes and middleware. 
+You can place this in your express routes, then comment them out when you don't need them any more. 
+
+*/
+
+
+const holler = (request, response, next) => {
+  
+  console.log('\n \nHoller!');
+  
+  //request.body
+  if(request.hasOwnProperty('body')){
+    console.log('request body is...', request.body, typeof request.body)
+  }else{
+    console.log('There is no request.body');
+  }
+
+  //request.params
+  if(request.hasOwnProperty('params')){
+    console.log('request params: ', request.params, typeof request.params)
+  }else{
+    console.log('There is no request.params');
+  }
+
+  //request.query
+  if(request.hasOwnProperty('query')){
+    console.log('request query: ', request.query, typeof request.query)
+  }else{
+    console.log('There is no request.query');
+  }
+
+  //response.locals
+  if(response.hasOwnProperty('locals')){
+    console.log('response locals: ', response.locals, typeof response.locals)
+  }else{
+    console.log('There is no response.locals');
+  }
+
+  //response.body
+  if(response.hasOwnProperty('body')){
+    console.log('response body: ', response.body, typeof response.body)
+  }else{
+    console.log('There is no response.body');
+  }
+
+  console.log('\n\nEnd Holler')
+  return next();
+}
+
+module.exports = holler;
